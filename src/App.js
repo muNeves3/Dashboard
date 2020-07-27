b8d1289811c101b811a3f10a35d42406168352c7
muNeves3 <murilogrilosn@gmail.com>
2020-07-27 19:36:05 -0300

    the colors were added

--- src/App.js
+++ src/App.js
@@ -14,18 +14,21 @@ function App() {
           imgsrc="https://app.rocketseat.com.br/static/media/Starter.64c237cc.svg"
           description="Torne-se um programador desejado"
           description1=" no mercado com esses cursos gratuitos"
+          color="#7159C1"
         />
         <Courses
           num="02"
           imgsrc="https://app.rocketseat.com.br/static/media/LaunchBase.08285320.svg"
           description="Domine a programação do zero"
           description1="e tenha acesso às melhores oportunidades do mercado"
+          color="#FD951F"
         />
         <Courses
           num="03"
           imgsrc="https://app.rocketseat.com.br/static/media/GoStack.83a178a0.svg"
           description="Treinamento imersivo"
           description1="nas tecnologias mais modernas de desenvolvimento web e mobile"
+          color="#04D361"
         />
       </div>
     </div>
